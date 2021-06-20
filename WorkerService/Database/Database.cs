058f9acd997c5086531e60629d352bdba61b6613
igor.ribeiro <igorapalvesribeiro@gmail.com>
2021-06-20 11:10:50 -0300

    criando classe Database

---
+++ WorkerService/Database/Database.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+using Microsoft.Extensions.Configuration;
+using WebsiteStatus;
+using Microsoft.Extensions.Logging;
+
+namespace WorkerService
+{
+    public class Database
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Database(ILogger<Worker> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public DataTable ExecuteConsult()
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+
+                //builder.DataSource = "localhost\\SQLEXPRESS";
+                //builder.UserID = "sa";
+                //builder.Password = "Saobento21";
+                //builder.InitialCatalog = "Teste";
+
+                builder.ConnectionString = Configuration.GetConnectionString("TesteDB");
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    connection.Open();
+
+                    String sql = "exec buscaDados";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+
+                            dt.Load(reader);
+                            for (int i = 0; i < dt.Rows.Count; i++)
+                            {
+                                var id = dt.Rows[i]["id"].ToString();
+                                var nome = dt.Rows[i]["nome"].ToString();
+                                var email = dt.Rows[i]["email"].ToString();
+                                string frase = "o usuário de id: " + id + " e de nome: " + nome + " e email: " + email;
+
+                                _logger.LogInformation(frase);
+
+
+                                //sql = "exec pr_inserirDados @id= " + Convert.ToInt32(id) + " , @nome= '" + nome + "' , @email= '" + email + "'";
+                                //SqlCommand commandInsert = new SqlCommand(sql, connection);
+                                //commandInsert.ExecuteNonQuery();
+
+
+                            }
+                            //_logger.LogInformation("{0}", reader.GetString(0));
+
+                            connection.Close();
+
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation(ex.ToString());
+            }
+
+            return dt;
+        }
+    }
+}
--- WorkerService/Worker.cs
+++ WorkerService/Worker.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using System.Text;
+using WorkerService;
 
 namespace WebsiteStatus
 {
@@ -18,7 +19,7 @@ namespace WebsiteStatus
         private readonly ILogger<Worker> _logger;
         private HttpClient client;
 
-        public Worker(ILogger<Worker> logger, IConfiguration configuration)
+        public Worker(ILogger<Worker> logger,IConfiguration configuration)
         {
             _logger = logger;
             Configuration = configuration;
@@ -38,69 +39,14 @@ namespace WebsiteStatus
             return base.StopAsync(cancellationToken);
         }
 
-        public DataTable ExecuteConsult()
-        {
-            DataTable dt = new DataTable();
-            try
-            {
-                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-
-                //builder.DataSource = "localhost\\SQLEXPRESS";
-                //builder.UserID = "sa";
-                //builder.Password = "Saobento21";
-                //builder.InitialCatalog = "Teste";
-
-                builder.ConnectionString = Configuration.GetConnectionString("TesteDB");
-
-                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
-                {
-                    connection.Open();
-
-                    String sql = "exec buscaDados";
-
-                    using (SqlCommand command = new SqlCommand(sql, connection))
-                    {
-                        using (SqlDataReader reader = command.ExecuteReader())
-                        {
-
-                            dt.Load(reader);
-                            for (int i = 0; i < dt.Rows.Count; i++)
-                            {
-                                var id = dt.Rows[i]["id"].ToString();
-                                var nome = dt.Rows[i]["nome"].ToString();
-                                var email = dt.Rows[i]["email"].ToString();
-                                string frase = "o usuário de id: " + id + " e de nome: " + nome + " e email: " + email;
-
-                                _logger.LogInformation(frase);
-
-
-                                //sql = "exec pr_inserirDados @id= " + Convert.ToInt32(id) + " , @nome= '" + nome + "' , @email= '" + email + "'";
-                                //SqlCommand commandInsert = new SqlCommand(sql, connection);
-                                //commandInsert.ExecuteNonQuery();
-
-
-                            }
-                            //_logger.LogInformation("{0}", reader.GetString(0));
-
-                            connection.Close();
-
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                _logger.LogInformation(ex.ToString());
-            }
-
-            return dt;
-        }
+       
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                DataTable consult = ExecuteConsult();
+                Database db = new Database(_logger,Configuration);
+                DataTable consult = db.ExecuteConsult();
 
                 string json = DataTable_JSON_StringBuilder(consult);
 
@@ -134,7 +80,7 @@ namespace WebsiteStatus
                 //_logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 await Task.Delay(60000 * 5, stoppingToken);
             }
-        }
+        }       
 
         public static string DataTable_JSON_StringBuilder(DataTable tabela)
         {
