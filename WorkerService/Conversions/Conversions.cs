598d3e98c2ba948f9a94815c53363dde982b3e54
igor.ribeiro <igorapalvesribeiro@gmail.com>
2021-06-20 11:29:28 -0300

    criando Conversions

---
+++ WorkerService/Conversions/Conversions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+namespace WorkerService.Convert
+{
+    public class Conversions
+    {
+        public Conversions()
+        {
+
+        }
+        public string DataTable_JSON_StringBuilder(DataTable tabela)
+        {
+            var JSONString = new StringBuilder();
+            if (tabela.Rows.Count > 0)
+            {
+                JSONString.Append("[");
+                for (int i = 0; i < tabela.Rows.Count; i++)
+                {
+                    JSONString.Append("{");
+                    for (int j = 0; j < tabela.Columns.Count; j++)
+                    {
+                        if (j < tabela.Columns.Count - 1)
+                        {
+                            JSONString.Append("\"" + tabela.Columns[j].ColumnName.ToString() + "\":" + "\"" + tabela.Rows[i][j].ToString() + "\",");
+                        }
+                        else if (j == tabela.Columns.Count - 1)
+                        {
+                            JSONString.Append("\"" + tabela.Columns[j].ColumnName.ToString() + "\":" + "\"" + tabela.Rows[i][j].ToString() + "\"");
+                        }
+                    }
+                    if (i == tabela.Rows.Count - 1)
+                    {
+                        JSONString.Append("}");
+                    }
+                    else
+                    {
+                        JSONString.Append("},");
+                    }
+                }
+                JSONString.Append("]");
+            }
+            return JSONString.ToString();
+
+        }
+    }
+}
--- WorkerService/Worker.cs
+++ WorkerService/Worker.cs
@@ -2,15 +2,13 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Data;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using System.Text;
 using WorkerService;
+using WorkerService.Convert;
 
 namespace WebsiteStatus
 {
@@ -43,78 +41,34 @@ namespace WebsiteStatus
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            Database db = new Database(_logger, Configuration);
+            Conversions conversions = new Conversions();
+
             while (!stoppingToken.IsCancellationRequested)
-            {
-                Database db = new Database(_logger,Configuration);
+            {                
                 DataTable consult = db.ExecuteConsult();
 
-                string json = DataTable_JSON_StringBuilder(consult);
+                string json = conversions.DataTable_JSON_StringBuilder(consult);                
 
                 var stringContent = new StringContent(json, Encoding.UTF8, "application/json");
-
-                var url = "https://www.iamtimcorey.com";
+                
                 var urlApi = "http://localhost:3333/post";
+                
+                var result = await client.PostAsync(urlApi, stringContent);
 
-
-                var result = await client.GetAsync(url);
-                var postResult = await client.PostAsync(urlApi, stringContent);
-
-                if (postResult.IsSuccessStatusCode)
+                if (result.IsSuccessStatusCode)
                 {
                     _logger.LogInformation("Dados enviados com sucesso");
                 }
                 else
                 {
-                    _logger.LogInformation("Ocorreu um erro ao enviar os dados. Status Code {StatusCode}", postResult.StatusCode);
+                    _logger.LogInformation("Ocorreu um erro ao enviar os dados. Status Code {StatusCode}", result.StatusCode);
                 }
 
-
-                if (result.IsSuccessStatusCode)
-                {
-                    _logger.LogInformation("O website está no ar. Status Code {StatusCode}", result.StatusCode);
-                }
-                else
-                {
-                    _logger.LogInformation("O website está fora do ar. Status Code {StatusCode}", result.StatusCode);
-                }
-                //_logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 await Task.Delay(60000 * 5, stoppingToken);
             }
         }       
 
-        public static string DataTable_JSON_StringBuilder(DataTable tabela)
-        {
-            var JSONString = new StringBuilder();
-            if (tabela.Rows.Count > 0)
-            {
-                JSONString.Append("[");
-                for (int i = 0; i < tabela.Rows.Count; i++)
-                {
-                    JSONString.Append("{");
-                    for (int j = 0; j < tabela.Columns.Count; j++)
-                    {
-                        if (j < tabela.Columns.Count - 1)
-                        {
-                            JSONString.Append("\"" + tabela.Columns[j].ColumnName.ToString() + "\":" + "\"" + tabela.Rows[i][j].ToString() + "\",");
-                        }
-                        else if (j == tabela.Columns.Count - 1)
-                        {
-                            JSONString.Append("\"" + tabela.Columns[j].ColumnName.ToString() + "\":" + "\"" + tabela.Rows[i][j].ToString() + "\"");
-                        }
-                    }
-                    if (i == tabela.Rows.Count - 1)
-                    {
-                        JSONString.Append("}");
-                    }
-                    else
-                    {
-                        JSONString.Append("},");
-                    }
-                }
-                JSONString.Append("]");
-            }
-            return JSONString.ToString();
-
-        }
+        
     }
 }
